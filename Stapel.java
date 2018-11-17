2657511cbd7c5af5e985f1958c0edfef82a60969
Sabine <11sloe@itg-dah.de>
2018-11-17 15:59:04 +0100

    Gemeinsames Projekt angelegt

---
+++ Stapel.java
@@ -0,0 +1,32 @@
+
+public class Stapel 
+{
+    
+  private Listenelement erster;
+  
+  public Stapel()
+  {
+      
+    }
+    
+    public void einfuegen(Kiste k)
+    {
+      
+    }
+    
+    public void ausgeben()
+    {
+        
+    }
+    
+    public boolean istLeer()
+    {
+        return false;
+    }
+    
+    public Kiste entnehmen()
+    {
+        
+        return null;
+    }
+}
